9414f44919288cde60b65f5e3c4a19d6410dfbff
Channy <chhunchannygod@gmail.com>
2025-10-16 16:57:33 +0700

    add: setup tailwind, router & others package

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { Contact, Home } from "lucide-react";
 import React from "react";
+import { Routes, Route } from "react-router";
 
 function App() {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/contact" element={<Contact />}></Route>
+    </Routes>
+  );
 }
 
 export default App;
